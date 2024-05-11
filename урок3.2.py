34e5a6c3e04654b506e31e1b1ed6dfc30aa1b37a
Dima-Ibragimov <168379280+Dima-Ibragimov@users.noreply.github.com>
2024-05-12 01:48:41 +0300

    Add files via upload

---
+++ урок3.2.py
@@ -0,0 +1,12 @@
+print('Здраствуйте, функция 1')
+def printer (a, b):
+    print(a, b)
+
+printer(80 , 'килограмм')
+
+print('Функция 2')
+
+def print_prams(f , v , k):
+    print(f, v, k)
+
+print_prams('Вам' , 20, "лет")
